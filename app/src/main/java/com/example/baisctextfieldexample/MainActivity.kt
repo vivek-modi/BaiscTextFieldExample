08f201c96a2173330c0d25fc1f911fb1c4206a32
Vivek Modi <vivekmodi456@gmail.com>
2025-01-20 19:55:04 +0000

    Initial commit

---
+++ app/src/main/java/com/example/baisctextfieldexample/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.baisctextfieldexample
+
+import android.os.Bundle
+import android.text.InputType
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.InterceptPlatformTextInput
+import androidx.compose.ui.platform.PlatformTextInputMethodRequest
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.baisctextfieldexample.databinding.MainActivityBinding
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val textFieldState = remember { TextFieldState() }
+            val interactionSource = remember { MutableInteractionSource() }
+
+            InterceptPlatformTextInput(
+                interceptor = { request, nextHandler ->
+                    val modifiedRequest = PlatformTextInputMethodRequest { outAttributes ->
+                        request.createInputConnection(outAttributes).also {
+                            outAttributes.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                        }
+                    }
+                    nextHandler.startInputMethod(modifiedRequest)
+                }
+            ) {
+                BasicTextField(
+                    state = textFieldState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(100.dp),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        autoCorrectEnabled = false,
+                    ),
+                    decorator = { view ->
+                        TextFieldDefaults.DecorationBox(
+                            value = textFieldState.text.toString(),
+                            innerTextField = view,
+                            enabled = true,
+                            singleLine = true,
+                            interactionSource = interactionSource,
+                            placeholder = {
+                                Text("Only Letter")
+                            },
+                            visualTransformation = VisualTransformation.None
+                        )
+                    }
+                )
+            }
+        }
+
+    }
+}
