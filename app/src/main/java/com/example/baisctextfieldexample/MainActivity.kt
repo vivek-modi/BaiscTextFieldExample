4229f7e94808ea9c93d0b0ae83333bcc84042179
Vivek Modi <vivekmodi456@gmail.com>
2025-01-24 13:06:34 +0000

    move to separate function

--- app/src/main/java/com/example/baisctextfieldexample/MainActivity.kt
+++ app/src/main/java/com/example/baisctextfieldexample/MainActivity.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicSecureTextField
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
@@ -27,7 +29,7 @@ import com.example.baisctextfieldexample.ui.theme.BaiscTextFieldExampleTheme
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,62 +37,68 @@ class MainActivity : ComponentActivity() {
             BaiscTextFieldExampleTheme {
                 val stateOne = rememberTextFieldState()
                 val stateOTwo = rememberTextFieldState()
-                Column(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(50.dp)
-                ) {
-                    BasicTextField(
-                        state = stateOne,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Email,
-                            imeAction = ImeAction.Next
-                        ),
-                        decorator = {
-                            TextFieldDefaults.DecorationBox(
-                                value = stateOne.text.toString(),
-                                innerTextField = it,
-                                enabled = true,
-                                singleLine = true,
-                                visualTransformation = VisualTransformation.None,
-                                label = {
-                                    Text("Username")
-                                },
-                                placeholder = {
-                                    Text("Username Placeholder")
-                                },
-                                interactionSource = remember { MutableInteractionSource() }
-                            )
-                        }
-                    )
-                    BasicSecureTextField(
-                        state = stateOTwo,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        decorator = {
-                            TextFieldDefaults.DecorationBox(
-                                value = stateOTwo.text.toString(),
-                                innerTextField = it,
-                                enabled = true,
-                                singleLine = true,
-                                visualTransformation = VisualTransformation.None,
-                                label = {
-                                    Text("Password")
-                                },
-                                placeholder = {
-                                    Text("Password Placeholder")
-                                },
-                                interactionSource = remember { MutableInteractionSource() }
-                            )
-                        }
+                BasicTextFieldExamples(stateOne, stateOTwo)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun BasicTextFieldExamples(stateOne: TextFieldState, stateOTwo: TextFieldState) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(50.dp)
+        ) {
+            BasicTextField(
+                state = stateOne,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                decorator = {
+                    TextFieldDefaults.DecorationBox(
+                        value = stateOne.text.toString(),
+                        innerTextField = it,
+                        enabled = true,
+                        singleLine = true,
+                        visualTransformation = VisualTransformation.None,
+                        label = {
+                            Text("Username")
+                        },
+                        placeholder = {
+                            Text("Username Placeholder")
+                        },
+                        interactionSource = remember { MutableInteractionSource() }
                     )
                 }
-            }
+            )
+            BasicSecureTextField(
+                state = stateOTwo,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                decorator = {
+                    TextFieldDefaults.DecorationBox(
+                        value = stateOTwo.text.toString(),
+                        innerTextField = it,
+                        enabled = true,
+                        singleLine = true,
+                        visualTransformation = VisualTransformation.None,
+                        label = {
+                            Text("Password")
+                        },
+                        placeholder = {
+                            Text("Password Placeholder")
+                        },
+                        interactionSource = remember { MutableInteractionSource() }
+                    )
+                }
+            )
         }
     }
 }
